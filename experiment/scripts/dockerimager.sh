7f1522a32d3a685664d033410f7b163a62060fd3
Danny Neelappa <Danny@Dannys-MBP.attlocal.net>
2020-03-05 16:26:55 -0500

    add private key

--- experiment/scripts/dockerimager.sh
+++ experiment/scripts/dockerimager.sh
@@ -77,6 +77,7 @@ if [[ "$CONTAINER_TYPE" == *"Docker" ]]; then
         -o BatchMode=yes $USERNAME@$host "
       sudo docker run --rm -p $POSTGRESQL_SSH:22 -p $POSTGRESQL_PORT:$POSTGRESQL_PORT -d --cpuset-cpus $POSTGRESQL_CPU --name postgresql harvardbiodept/nucleus
       sudo docker cp ~/.ssh/id_rsa.pub postgresql:/root/.ssh/
+      sudo docker cp ~/.ssh/id_rsa postgresql:/root/.ssh/
       sudo docker exec -w /root/.ssh postgresql bash -c "cat id_rsa.pub >> authorized_keys"
 
     " &
